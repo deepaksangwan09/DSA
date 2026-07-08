436c13d605aa2219353db068a0685f958f7cbd0d
deepaksangwan <sangwandeepak008@gmail.com>
2026-07-08 09:01:13 +0530

    Time: 2 ms (72.29%), Space: 10.7 MB (93.31%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    int find(vector<int>&a){
-        int maxc = -1;
-        for(int i=0; i<256; i++){
-            maxc = max(maxc, a[i]);
-        }
-        return maxc;
-    }
-
     int characterReplacement(string s, int k) {
-        int n = s.size();
-        vector<int> f(256,0);
+        vector<int>counts(26,0);
 
-        int low=0, high=0, res=INT_MIN;
+        int left=0; 
+        int max_count = 0;
+        int max_length=0;
 
-        for(int high =0; high<n; high++){
-            f[s[high]]++;
-            int maxcnt = find(f);
-            int len = high-low+1;
-            int diff = len-maxcnt;
+        for(int right=0; right<s.length(); right++){
+            counts[s[right] - 'A']++;
 
-            while(diff>k){
-                f[s[low]]--;
-                low++;
-                maxcnt = find(f);
-                len = high-low+1;
-                diff = len-maxcnt;
+            max_count = max(max_count, counts[s[right] - 'A']);
+
+            while((right-left+1) - max_count > k){
+                counts[s[left]-'A']--;
+                left++;
             }
-            len = high-low+1;
-            res = max(res, len);
-        }
-        return res;
-        
+
+            max_length = max(max_length, right-left+1);
+        } 
+        return max_length;  
     }
+    
 };
